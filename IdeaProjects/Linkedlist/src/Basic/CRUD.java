2074c1b0b7723d7c9c174f9821bd49a469ffd9b3
ranepratikk <iranepratik@gmail.com>
2025-11-13 18:35:59 +0530

    commit

---
+++ IdeaProjects/Linkedlist/src/Basic/CRUD.java
@@ -0,0 +1,87 @@
+package Basic;
+
+import Basic.CRUD_Node;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class CRUD
+{
+    public static void main(String[] args) {
+        LinkedList<CRUD_Node> list=new LinkedList<>();
+        Scanner in=new Scanner(System.in);
+        //insert
+        String name;String gender;int eid;int salary;
+        for(int i=0;i<2;i++) {
+            System.out.println("\nEnter name,gender,id,salary for record "+(i+1));
+            name = in.next();
+            gender = in.next();
+            eid = in.nextInt();
+            salary = in.nextInt();
+            CRUD_Node n = new CRUD_Node( name,  eid,  salary,  gender);
+            list.add(n);
+        }
+        //print series
+        for(CRUD_Node i:list)
+        {
+            i.print();
+        }
+
+        //search for id
+        System.out.println("Enter id to search:");
+        int id=in.nextInt();
+        boolean found=false;
+        for(CRUD_Node i:list)
+        {
+            if(i.eid==id)
+            {
+                System.out.println("Iyla found");
+                i.print();
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+            System.out.println("Not Found");
+
+
+
+
+
+        // UPDATE
+        System.out.println("Enter id to update:");
+        int hid = in.nextInt();
+        boolean updated = false;
+
+        for (CRUD_Node i : list) {
+            if (i.eid == hid) {
+                System.out.println("Employee found. Current details:");
+                i.print();
+
+                System.out.println("Enter new name:");
+                i.ename = in.next();
+
+                System.out.println("Enter new gender:");
+                i.gender = in.next();
+
+                System.out.println("Enter new salary:");
+                i.salary = in.nextInt();
+
+                System.out.println("Record updated ");
+                i.print();
+
+                updated = true;
+                break;
+            }
+        }
+
+        if (!updated)
+            System.out.println("Record not found for update.");
+    }
+}
+
+
+
+
+
+
---
+++ IdeaProjects/Linkedlist/src/Basic/CRUD_Node.java
@@ -0,0 +1,21 @@
+package Basic;
+
+public class CRUD_Node {
+    String ename;
+    public int eid;
+    String gender;
+    int salary;
+    CRUD_Node next;
+    public CRUD_Node(String name, int id, int salary, String gender){
+         ename=name;
+         eid=id;
+         this.salary=salary;
+         this.gender=gender;
+         next=null;
+
+
+    }
+    void print(){
+        System.out.println("NAME:"+ename+"id:"+eid+"salary:"+salary+"gender:"+gender);
+    }
+}
