8d60cd9ae13c43885037a7ae38c280878f84bc9f
ranepratikk <iranepratik@gmail.com>
2025-11-13 12:30:18 +0530

    final

--- IdeaProjects/Linkedlist/src/LinearLinkedList.java
+++ IdeaProjects/Linkedlist/src/LinearLinkedList.java
@@ -1,24 +1,170 @@
 public class LinearLinkedList {
     Node root;
-    void insert_left(int data){
-        Node n=new Node(data);
-        if(root==null){
-            root=n;
-        }
-        else{
-            n.next=root;
-            root=n;
+
+    void insert_left(int data) {
+        Node n = new Node(data);
+        if (root == null) {
+            root = n;
+        } else {
+            n.next = root;
+            root = n;
         }
     }
 
-    void delete_left(){
-        if(root==null){
+    void delete_left() {
+        if (root == null) {
             System.out.println("empty linkedlist");
+        } else {
+            Node t = root;
+            root = root.next;
+            System.out.println("deleted:" + t.data);      //Response msg just to let know its deleted
+        }
+    }
+
+    void insert_right(int data) {
+        Node n = new Node(data);
+        if (root == null) {
+            root = n;
+        } else {
+            Node t = root;
+            while (t.next != null)
+                t = t.next;
+            t.next = n;
+        }
+    } // close insert_right
+
+    void delete_right() {
+        //tail method-trailer method
+        //make temp available for previous of link
+
+        if (root == null) {
+            System.out.println("empty linkedlist");
+        } else {
+
+            Node t = root;
+            Node t2 = root;
+
+            while (t.next != null) {
+                t2 = t;
+                t = t.next;
+
+            }
+
+
+            if (root.next== null) {
+                root = null;
+            } else {
+                t2.next = null;
+                System.out.println("Deleted" + t.data);
+            }
+        }
+
+    }
+    void print(){
+        if(root==null){
+            System.out.println("List Empty");
         }
         else{
             Node t=root;
-            root=root.next;
-            System.out.println("deleted:"+t.data);      //Response msg just to let know its deleted
+            while(t!=null){
+                System.out.print("-"+t.data+"-");
+                t=t.next;
+            }
+        }
+
+    }
+    void search(int key){
+        if(root==null){
+            System.out.println("list is empty");
+        }
+        else{
+            Node t=root;
+            while(t!=null){
+                if(key==t.data){
+                    System.out.println("Element Found");
+                    return;
+                }
+                t=t.next;
+            }
         }
     }
+
+    void delete(int key){
+        if(root==null){
+            root=null;
+        }
+        Node t1 = root,t2=root;
+        while(t1!=null){
+            if(t1.data==key){
+                System.out.println("found"+key);
+                break;
+            }
+            t2=t1;
+           t1= t1.next;
+
+        } if (t1==null){
+            System.out.println("not found"+key);
+        }
+        else{
+            System.out.println("found"+key);
+            if(t1==root){
+               root=root.next;
+            } else if (t1.next==null) {
+                t2.next=null;
+            }
+            else t2.next= t1.next;
+            System.out.println(t1.data+"deleted");
+
+        }
+    }
+    void insert_after(int key, int data)
+    {
+        if (root == null) {
+            System.out.println("List is empty");
+        }
+        else {
+            Node t;
+            t = root;                // t = root
+
+            while (t != null) {      // if found, stop.
+                if (t.data == key)
+                    break;
+                t = t.next;
+            }
+
+            if (t == null) {         // not found
+                System.out.println("\n" + key + " Not Found");
+            }
+            else {
+                System.out.println("\n" + key + " Found");
+                Node n = new Node(data);   // created new node
+
+                // insertion part
+                n.next = t.next;     // n ref to whoever is next
+                t.next = n;          // t ref to n
+
+                System.out.println("\n" + t.data + " deleted.");
+            }
+        }
+    }
+    void sort_list()
+    {
+        if(root==null)
+            System.out.println("Empty list");
+        else
+        {
+            for(Node i=root;i.next!=null;i=i.next)//for passes
+            {
+                for(Node t=root,t2=t.next;t2!=null;t=t.next,t2=t2.next)//sorts
+                {
+                    if(t.data>t2.data)
+                    {
+                        int temp=t.data;t.data=t2.data;t2.data=temp;
+                    }
+                }
+            }
+        }
+        System.out.println("\nSorting done..");
+    }
+
 }
--- IdeaProjects/Linkedlist/src/Main.java
+++ IdeaProjects/Linkedlist/src/Main.java
@@ -1,15 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+     LinearLinkedList ll=new LinearLinkedList();
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("\n====== Linkedlist ======");
+            System.out.println("1. Insert Right");
+            System.out.println("2. Insert Left");
+            System.out.println("3. Delete Left");
+            System.out.println("4. Delete Right");
+            System.out.println("5. Print ");
+            System.out.println("6. Search ");
+            System.out.println("7. Delete ");
+            System.out.println("8. Insert After ");
+            System.out.println("9. Sort ");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter value ");
+                    int val = sc.nextInt();
+                     ll.insert_left(val);
+                    System.out.println(val+":value inserted");
+                    break;
+
+                case 2:
+                    System.out.print("Enter value ");
+                    int hal = sc.nextInt();
+                    ll.insert_left(hal);
+                    System.out.println(hal+":value inserted");
+                    break;
+
+
+                case 3:
+                    ll.delete_left();
+
+
+                    break;
+
+                case 4:
+                    ll.delete_right();
+
+
+                    break;
+
+                case 5:
+                    ll.print();
+                    break;
+                case 6:
+                    int h=sc.nextInt();
+                    ll.search(h);
+                    break;
+                    case 7:
+                        int val1= sc.nextInt();
+                        ll.search(val1);
+                        break;
+                case 8:
+                    System.out.println("Insert data");
+                    int val2= sc.nextInt();
+                    System.out.println("Insert key");
+
+                    int key=sc.nextInt();
+                    ll.insert_after(key,val2);
+                    break;
+                case 9:
+
+                    ll.sort_list();
+                    break;
+
+
+
+
+                default:
+                    System.out.println("Check option again ");
+            }
+        } while (choice != 0);
+
+        sc.close();
     }
 }
+
